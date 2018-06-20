2bf50e51a9b6e9a08a39988761496cfd38ce8f88
talktobrent <talktobrent@gmail.com>
2018-06-20 22:23:25 +0000

    C source for printing character string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  prints string to standard output
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
