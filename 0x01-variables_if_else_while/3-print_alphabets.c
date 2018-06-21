3f3f313b2a1016a88a72cfe1beece457be40ed40
talktobrent <talktobrent@gmail.com>
2018-06-21 23:51:50 +0000

    Created source to print alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*  main - print alphaBET
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	x = 97;
+
+	while (x < 123)
+		{
+		putchar(x);
+		x++;
+		}
+	x = 65;
+	while (x < 91)
+		{
+		putchar(x);
+		x++;
+		}
+	putchar(10);
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
