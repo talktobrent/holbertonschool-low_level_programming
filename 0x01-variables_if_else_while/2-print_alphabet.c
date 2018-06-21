20620a2141544d8d154265b2fde233f07d2e29b1
talktobrent <talktobrent@gmail.com>
2018-06-21 22:19:12 +0000

    Created putchar loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*  main - Display last digit of
+* random number
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	x = 97;
+
+	while (x < 123)
+		{
+		putchar (x);
+		x++;
+		}
+	putchar(10);
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
