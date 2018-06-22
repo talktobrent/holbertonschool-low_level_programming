2da3127bab24b32a478f3ce46115eec0311eed99
talktobrent <talktobrent@gmail.com>
2018-06-22 04:35:15 +0000

    Created source to print 00-99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*  main - print 00-99
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+	int h;
+
+	x = '0';
+	h = '0';
+
+	while (x <= '9')
+	{
+		h = '0';
+
+		while (h <= '9')
+		{
+			putchar(x);
+			putchar(h);
+			if (x == '9' && h == '9')
+				putchar(10);
+			putchar(',');
+			putchar(' ');
+			h++;
+		}
+
+		x++;
+	}
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
