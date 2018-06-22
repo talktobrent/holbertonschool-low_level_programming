eff9c5962b443c6631b5cdd80445743cc7a9d45c
talktobrent <talktobrent@gmail.com>
2018-06-22 01:27:19 +0000

    Source to print numbers with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - print numbers with commas
+* and spaces
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+	int h;
+
+	x = 48;
+	h = 48;
+
+	while (x < 58)
+		{
+		putchar(x);
+		if (x == 57)
+			break;
+		else
+		{
+		putchar(44);
+		putchar(32);
+		x++;
+		}
+		}
+
+	putchar(10);
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
