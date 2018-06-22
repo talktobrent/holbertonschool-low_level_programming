a2f6e216b63b48acc714401cc35d100e693b3c98
talktobrent <talktobrent@gmail.com>
2018-06-22 00:11:27 +0000

    Created source that prints 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*  main - print alphaBET
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+
+	x = 48;
+
+	while (x < 58)
+		{
+		putchar(x);
+		x++;
+		}
+	putchar(10);
+
+	return (0);
+}
+
+
+
+
+
+
+
+
