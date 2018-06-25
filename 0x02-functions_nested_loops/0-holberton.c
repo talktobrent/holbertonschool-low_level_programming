a5c8ff31d6876dfcd4c21a849d6cc024b1b20086
talktobrent <talktobrent@gmail.com>
2018-06-25 23:47:15 +0000

    Created source with user header

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**Prints 'Holberton' 
+*using _putchar
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{	
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
+
+	return(0);
+}
