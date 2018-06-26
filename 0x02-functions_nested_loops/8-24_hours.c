90276f64d011583baf40026b548673a3559309d1
talktobrent <talktobrent@gmail.com>
2018-06-26 22:32:00 +0000

    Prototype prints every min of 24 hr

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+* jack_bauer - print every min
+*
+* Description: prints every min in 24 hours
+* Return: always 0
+*/
+
+void jack_bauer(void)
+{
+	int x, y, a, b, c, d;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			a = x / 10;
+			b = x % 10;
+			c = y / 10;
+			d = y % 10;
+
+			_putchar(a + '0');
+			_putchar(b + '0');
+			_putchar(':');
+			_putchar(c + '0');
+			_putchar(d + '0');
+			_putchar(10);
+		}
+	}
+}
+
+
+
+
+
+
