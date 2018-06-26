4a7c483d047f33240a0c2ae7435ab456fc6b81bf
talktobrent <talktobrent@gmail.com>
2018-06-26 23:00:57 +0000

    Prototype that adds to numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/**
+* add - add two ints
+* @x: first operand
+* @y: second operand
+*
+* Description: adds two integers
+* Return: sum of two integers
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+
+
+
+
