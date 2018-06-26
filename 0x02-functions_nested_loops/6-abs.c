5daa45e3d2a293ac8696bdeb866c47425db9fa9f
talktobrent <talktobrent@gmail.com>
2018-06-26 19:09:36 +0000

    Fixed style, and added comments

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,20 @@
 
 
 /**
-* int_abs - computes 
-*absolute value
+* _abs - absolute value
+* @x: number checked
 *
-* Returns 1 or 0
+* Description: extracts abolute value
+* Return: absolute value of x
 */
 
 int _abs(int x)
 {
-	if(x >= 0)
+	if (x >= 0)
 		(x = x);
-	
+
 	else
 	(x = (x / -1));
-	
-	return(x);
+
+	return (x);
 }
