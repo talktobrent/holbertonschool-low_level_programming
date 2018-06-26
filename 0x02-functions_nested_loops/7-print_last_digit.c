9f8f65fd4e542643c6e5fd81714b6a598e6ddf54
talktobrent <talktobrent@gmail.com>
2018-06-26 21:10:07 +0000

    Created prototype to print last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_last_digit - print last digit
+* @x: character tested
+*
+* Description: gets last digit of any number
+* Return: value of x
+*/
+
+int print_last_digit(int x)
+{
+	if (x < 0)
+		x = (x * -1);
+	x  = (x % 10);
+	x  = (x + 48);
+	_putchar(x);
+	return (x - 48);
+
+}
