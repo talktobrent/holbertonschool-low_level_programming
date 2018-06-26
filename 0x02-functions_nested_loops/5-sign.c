32d1ad6f5ce5592c07ffe85ad2a7dd57f5ce82fb
talktobrent <talktobrent@gmail.com>
2018-06-26 19:04:03 +0000

    Formatted and added comments, and fixed style errors

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 /**
-* print_sign - prints
-*sign of number
+* print_sign - prints sign of int
+* @n: signed integer to be checked
 *
-* Returns 1 , 0 , or -1
+* Description: prints + - or 0 and  signed int
+* Return: 1 of pos, -1 if neg, 0 if 0
 */
 
 int print_sign(int n)
 {
-	
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
-		_putchar('0');	
-		return(0);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
