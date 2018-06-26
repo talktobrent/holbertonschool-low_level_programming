5e4020a637991193f414715f637bea43e060948f
talktobrent <talktobrent@gmail.com>
2018-06-26 19:20:10 +0000

    Fixed style and added comments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
-
 /**
-* isalpha - checks for 
-*alphabetic char
+* _isalpha - checks for alpha char
+* @c: character tested
 *
-* Returns 1 or 0
+* Description: tests ASCII range to find alpha char
+* Return: 1 or 0
 */
 
 int _isalpha(int c)
 {
-	
-	if((c > 96 && c < 123) || (c > 64 && c < 91))
-		return(1);
-	
-	return(0);
+
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
+		return (1);
+
+	return (0);
 }
