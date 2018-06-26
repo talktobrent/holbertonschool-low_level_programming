01fb84bb33700425da5ef22023b5702e88ba71f8
talktobrent <talktobrent@gmail.com>
2018-06-26 21:35:17 +0000

    Changed subtraction operation placement

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-
+#include "holberton.h"
 /**
 * print_last_digit - print last digit
 * @x: character tested
@@ -9,9 +9,9 @@
 
 int print_last_digit(int x)
 {
+	x  = (x % 10);
 	if (x < 0)
 		x = (x * -1);
-	x  = (x % 10);
 	x  = (x + 48);
 	_putchar(x);
 	return (x - 48);
