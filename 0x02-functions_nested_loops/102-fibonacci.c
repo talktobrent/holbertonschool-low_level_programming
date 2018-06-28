c83bbc18f3659445dfbae46a598e837e89c43d0f
talktobrent <talktobrent@gmail.com>
2018-06-28 01:29:59 +0000

    Main that prints fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Prints fibonacci
+*
+* Description: Prints first 50 fibonacci numbers
+*/
+
+int main(void)
+{
+long int t, x, y, z;
+
+x = 0;
+y = 1;
+	for (t = 0; t < 50; t++)
+	{
+
+		z = x + y;
+		printf("%li", z);
+		x = y;
+		y = z;
+		if (t != 49)
+		{
+		printf(", ");
+		}
+	}
+printf("\n");
+return (0);
+}
