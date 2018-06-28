4bd5e0dcc69f3d997d2cfb4f54b746c2b0a504fc
talktobrent <talktobrent@gmail.com>
2018-06-28 19:55:10 +0000

    Prototype to multiply two digits

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+* _mul - multiply
+* @a: First operand
+* @b: Second operand
+*
+* Description: multiplies two digits
+* Return: product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
