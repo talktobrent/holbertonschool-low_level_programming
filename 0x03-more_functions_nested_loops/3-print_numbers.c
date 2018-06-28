abbf7559f978634eb18a730606cc9696d7e1498b
talktobrent <talktobrent@gmail.com>
2018-06-28 20:35:33 +0000

    Prototype prints 0-9

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_numbers - prints 0-9
+*/
+
+void print_numbers(void)
+{
+	int zton;
+
+	for (zton = 48; zton < 58; zton++)
+		_putchar(zton);
+
+	_putchar(10);
+}
