3f47ab08b95e44099300dd7d6b74a54a6f27c02f
talktobrent <talktobrent@gmail.com>
2018-06-29 00:04:42 +0000

    Main finds multiples of 3 and 5

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - finds multiples of 3 and 5
+*
+* Description: Prints 'Fizz' for multiples of 3,
+* Buzz for multiples of 5
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+int num;
+	for (num = 1; num < 101; num++)
+	{
+		if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", num);
+		if (num < 100)
+			printf(" ");
+	}
+printf("\n");
+return (0);
+}
