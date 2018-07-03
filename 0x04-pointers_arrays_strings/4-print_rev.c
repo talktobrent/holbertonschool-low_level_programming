1c42ab899f76bb25d7e19701f466638b26aea744
talktobrent <talktobrent@gmail.com>
2018-07-03 03:43:11 +0000

    Created prototype

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_rev - prints string in reverse
+* @str: string to print
+* Description: prints string, in reverse, plus new line, to stdout
+*/
+
+void print_rev(char *str)
+{
+int count;
+
+while (*str > 0)
+{
+	str = str + 1;
+	count++;
+}
+while (count > 0)
+{
+	_putchar(*str);
+	str = str - 1;
+	count--;
+}
+
+_putchar(10);
+
+}
+
