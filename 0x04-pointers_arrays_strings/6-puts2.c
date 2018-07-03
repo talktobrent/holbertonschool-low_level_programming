69ff20b0a3bea031630d2ccc5422e6c9c596adee
talktobrent <talktobrent@gmail.com>
2018-07-03 22:48:39 +0000

    Created prototype

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* puts2 - prints every other char
+* @str: string to print
+* Description: prints every other char in string, starting with first
+*/
+
+void puts2(char *str)
+{
+int count;
+
+count = 1;
+
+while (*str != 0)
+{
+	str = str + 1;
+
+	if ((count % 2) != 0)
+		_putchar(*str);
+
+	count++;
+}
+_putchar(10);
+}
