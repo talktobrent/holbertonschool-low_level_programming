bd65dc4e0291b2abac575f6ee8a4c9b5bc037c81
talktobrent <talktobrent@gmail.com>
2018-07-04 02:01:29 +0000

    Deleted redundant variables, changed loop counts for accuracy, and changed loop conditons to elinimate NULL char

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -7,34 +7,33 @@
 
 void puts_half(char *str)
 {
-int run, count, print;
+int count, print;
 
 
-print = 1; /* counter to print to end of string*/
-count = 1; /* counts length of string*/
+print = 0; /* counter to print to end of string*/
+count = 0; /* counts length of string*/
 
-while (*str > 0)
+while (*str != 0)
 {
 	str = str + 1;
 	count++;
 }
 
-run = count - 1;  /*stores total length*/
 str = str - count; /*resets location to beginning of string */
 
-if (run % 2 == 0)
+if (count % 2 == 0)
 {
-	while (print <= run)
+	while (str[(count / 2 + print)] != 0)
 	{
-		_putchar(str[(run / 2 + print)]);
+		_putchar(str[(count / 2 + print)]);
 		print++;
 	}
 }
 else
 {
-	while (print <= run)
+	while (str[(count + 1) / 2 + print] != 0)
 	{
-		_putchar(str[(run - 1) / 2 + print]);
+		_putchar(str[(count + 1) / 2 + print]);
 		print++;
 	}
 }
