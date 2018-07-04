ec9eedfacc0a03e944c0f59c8420132e9fa6b57a
talktobrent <talktobrent@gmail.com>
2018-07-04 03:48:29 +0000

    Created prototype

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+/**
+* *_strcpy - copies string
+* @src: string to be copied
+* @dest: copied string goes here
+* Return: value at dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int count;
+
+for (count = 0; src[count] >= 0; count++)
+	dest[count] = src[count];
+
+return (dest);
+}
+
