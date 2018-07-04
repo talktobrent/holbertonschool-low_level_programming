4b6ec39352705fa521d65048639e7f2774a23bdf
talktobrent <talktobrent@gmail.com>
2018-07-04 05:10:28 +0000

    Changed 'for' loop to stop before 'null', considering 'null' is automatically added by compiler

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,10 @@ char *_strcpy(char *dest, char *src)
 {
 int count;
 
-for (count = 0; src[count] >= 0; count++)
+for (count = 0; src[count] != 0; count++)
+{
 	dest[count] = src[count];
-
+}
 return (dest);
 }
 
