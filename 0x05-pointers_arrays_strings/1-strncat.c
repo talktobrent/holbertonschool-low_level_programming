f8cc37c3e72aaa61320a4f36b9a5395fbdb0f566
talktobrent <talktobrent@gmail.com>
2018-07-06 22:13:27 +0000

    Created prototype

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+/**
+* _strncat - Concatenates two strings
+* @dest: string to be appended upon
+* @src: string to appended to dest
+* @n; bytes (length) from source to be copied
+* Description: Concatenates two strings, src up to n added to dest
+* Return: Pointer to dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int dest_count, src_count;
+
+dest_count = 0;
+
+while (dest[dest_count] != 0)
+	dest_count++;
+
+for (src_count = 0; src[src_count] != 0 && src_count < n; src_count++)
+	dest[dest_count++] = src[src_count];
+
+dest[dest_count] = 0;
+
+return (dest);
+}
