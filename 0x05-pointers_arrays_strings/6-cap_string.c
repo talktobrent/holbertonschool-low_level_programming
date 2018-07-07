7d8f3e913e226eb14c5148b3e7a8be05d5d8a6ac
talktobrent <talktobrent@gmail.com>
2018-07-07 22:34:38 +0000

    Created prototype

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/**
+* cap_string - capitalize of all words
+* @s: string to be changed
+* Description: capitalizes first letter of all words
+* Return: modified string
+*/
+
+char *cap_string(char *s)
+{
+int count;
+	for (count = 0; s[count] != 0; count++)
+	{
+		if (s[count] > 96 && s[count] < 123)
+		{
+			switch (s[count - 1])
+			case 10:
+			case 32:
+			case 9:
+			case ',':
+			case ':':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			s[count] = s[count] - 32;
+		}
+
+	}
+return (s);
+}
+
