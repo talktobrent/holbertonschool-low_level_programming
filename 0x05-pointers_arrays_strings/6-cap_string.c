a8c83fbbc5e7b64b28cfb4707bf6af6d55e6e39d
talktobrent <talktobrent@gmail.com>
2018-07-07 23:35:23 +0000

    Removed 'if' statement replacing tabs, and added condition to not change lowercase on first char of string

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,9 @@ char *cap_string(char *s)
 int count;
 	for (count = 0; s[count] != 0; count++)
 	{
-		if (s[count] > 96 && s[count] < 123)
+		if (count > 0 && (s[count] > 96 && s[count] < 123))
 		{
 			switch (s[count - 1])
-			{
 			case 10:
 			case 32:
 			case 9:
@@ -28,9 +27,6 @@ int count;
 			case '{':
 			case '}':
 			s[count] = s[count] - 32;
-			}
-			if (s[count - 1] == 9)
-				s[count - 1] = 32;
 		}
 	}
 return (s);
