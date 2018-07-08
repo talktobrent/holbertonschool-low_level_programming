a4a91cae31b397ebe8b72558699b241ba24cc0d8
talktobrent <talktobrent@gmail.com>
2018-07-08 05:13:00 +0000

    Changed 'src_count' increment location in last while loop, and removed 'n = n - 1' adjustment

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,18 @@ char *_strncpy(char *dest, char *src, int n)
 
 int src_count;
 
+
 for (src_count = 0;  src[src_count] != 0; src_count++)
 {
 	if (src_count < n)
 		dest[src_count] = src[src_count];
 }
 
-while (n - 1 > src_count)
-	dest[src_count++] = 0;
-
+while (n > src_count)
+{
+	dest[src_count] = 0;
+	src_count++;
+}
 
 return (dest);
 }
