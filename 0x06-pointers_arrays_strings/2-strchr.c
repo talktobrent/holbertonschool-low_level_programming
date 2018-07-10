4a5e09c6a11dbf9bec191d4ba0bf5ac50c1c9702
talktobrent <talktobrent@gmail.com>
2018-07-10 17:51:35 +0000

    Removed braces contained in for loop

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,9 @@ char *_strchr(char *s, char c)
 {
 int count;
 
-	for (count = 0; s[count] != 0; count++)
-	{
-		if (s[count] == c)
-			return (s + count);
-	}
+for (count = 0; s[count] != 0; count++)
+	if (s[count] == c)
+		return (s + count);
 
 return (s + count);
 }
