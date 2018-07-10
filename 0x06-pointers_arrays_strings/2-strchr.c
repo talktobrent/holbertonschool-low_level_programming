42996b67269e68d1cac6ce16f726bc4bbece9e01
talktobrent <talktobrent@gmail.com>
2018-07-10 17:08:59 +0000

    Created prototype

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+* _strchr - locates a character in a string
+* @s: string to be searched
+* @c: character to be looked for
+*
+* Description: searches for c character in a string
+* Return: pointer to first occurance of char in string
+*/
+
+char *_strchr(char *s, char c)
+{
+int count;
+
+	for (count = 0; s[count] != 0; count++)
+	{
+		if (s[count] == c)
+			return (s + count);
+	}
+
+return (s + count);
+}
