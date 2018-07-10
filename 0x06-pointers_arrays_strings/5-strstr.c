05f17e5df21b4566b130520c336068be9836bffd
talktobrent <talktobrent@gmail.com>
2018-07-10 22:37:34 +0000

    Created prototype

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+/**
+* _strstr - finds same string in another string
+* @haystack: string to be searched
+* @needle: string to search for
+*
+* Description: when the full string of needle is found in haystack
+* Return: beginning string match in haystack, plus the rest of haystack string
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int count, test;
+
+for (count = 0; haystack[count] != 0; count++)
+{
+	if (haystack[count] == needle[test])
+	{
+		while (haystack[count + test] == needle[test])
+		{	test++;
+			if (needle[test] == 0)
+				return (haystack + count);
+		}
+	}
+}
+return (0);
+}
