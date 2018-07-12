3936afc4911296889132bd64b3e1bf51f5ea9090
talktobrent <talktobrent@gmail.com>
2018-07-12 01:52:36 +0000

    Created prototype

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - value of x raised to the power of y
+* @x: number to be raised
+* @y: power of
+* Return: value of x
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+
+
+}
+
