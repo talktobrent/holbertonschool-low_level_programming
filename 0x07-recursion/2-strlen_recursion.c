b19f5bf2ec59b16a0dae69831831c2fe6810e84d
talktobrent <talktobrent@gmail.com>
2018-07-12 00:21:33 +0000

    Created prototype

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - gets length of string
+* @s: string to be tested
+* Return: length of string
+*/
+
+int _strlen_recursion(char *s)
+{
+
+
+	if (*s != 0)
+	return (1 + _strlen_recursion(s + 1));
+
+
+	else
+		return (0);
+}
+
