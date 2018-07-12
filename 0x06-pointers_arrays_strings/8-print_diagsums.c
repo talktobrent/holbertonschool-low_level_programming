174e8c691221285465e43bad4ea13a304c2905c5
talktobrent <talktobrent@gmail.com>
2018-07-12 22:59:54 +0000

    Created prototype

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* void print_diagsums(int *a, int size)
+* print_diagsums - prints sum of diagonals on 2d array
 * @a: array to use
 * @size: size of array
 * Description: prints sum of 2 opposite diagonals across center of a 2d array
@@ -11,29 +11,22 @@
 void print_diagsums(int *a, int size)
 {
 
-int count, sizer, sum;
-
-sizer = size - 1;
-
-for (count = 0; count < size; count++)
-{
-	sum  = sum + *(a * count * 4 * sizer * 4);
-	sizer--;
-}
-printf("%i", sum);
+int count, sum;
 
 sum = 0;
-for (; count >= 0; count--)
+for (count = 0; count < size; count++)
 {
-	sum = sum + *(a * count * 4 * sizer * 4);
-	sizer++;
+	sum  = sum + *(a + (count * size) + count);
 }
-printf("%i", sum);
+printf("%i, ", sum);
+
+sum = 0;
+for (count = size; count > 0; count--)
+{
+	sum = sum + *(a + (count * size) - count);
+}
+printf("%i\n", sum);
 
 }
-
-
-
-
 
 
