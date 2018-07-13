68e6e222ef0a148acd62c95c7dcc516f7860b0c1
talktobrent <talktobrent@gmail.com>
2018-07-13 04:27:00 +0000

    Added function declaration

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -19,4 +19,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int findpal(char *s, int count);
+
 #endif /* _holberton_h_ */
