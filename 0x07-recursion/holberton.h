6dbab1a959ad1b010c523bde2fe5cceaaaa622b5
talktobrent <talktobrent@gmail.com>
2018-07-13 04:23:24 +0000

    Added function declaration to header

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -12,5 +12,11 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char c);
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
 #endif /* _holberton_h_ */
