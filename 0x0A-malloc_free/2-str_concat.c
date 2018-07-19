4c2582a3856f4aceb4f1ec0ec52559d247fd3cbe
talktobrent <talktobrent@gmail.com>
2018-07-19 01:12:17 +0000

    Changed 'if' statements checking for NULL to change variables to empty () strings, rather than single ('\0') string

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -16,10 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int count2;
 
 	if (s1 == NULL)
-		s1[0] = '\0';
+		s1 = "";
 
 	if (s2 == NULL)
-		s2[0] = '\0';
+		s2 = "";
 
 	for (count = 0; s1[count] != '\0'; count++)
 	{
