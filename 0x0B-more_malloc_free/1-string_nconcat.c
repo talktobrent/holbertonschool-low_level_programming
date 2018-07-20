101ce50af9f36ad094d9f28676ec0edb7117b700
talktobrent <talktobrent@gmail.com>
2018-07-20 00:40:47 +0000

    Created function

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+
+/**
+* string_nconcat - concatenates two strings
+* @s1: string to be added to
+* @s2: string to be added
+* @n: length of s2 to add
+* Return: pointer to new combined string, NULL if fail
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+unsigned int length1, length2, place;
+char *newstring;
+
+if (s1 == NULL)
+	s1 = "";
+
+if (s2 == NULL)
+	s2 = "";
+
+length1 = 0;
+length2 = 0;
+
+while (s1[length1] != 0)
+	length1++;
+length1++;
+
+while (s2[length2] != 0)
+	length2++;
+length2++;
+
+if (n < length2)
+	newstring = malloc(sizeof(char) * (length1 + n + 1));
+
+else
+	newstring = malloc(sizeof(char) * (length1 + length2 + 1));
+
+if (newstring == NULL)
+{
+	free(newstring);
+	return (NULL);
+}
+
+for (place = 0; place < length1; place++)
+	newstring[place] = s1[place];
+
+length1--;
+if (n < length2)
+{
+	for (place = 0; place < n; place++)
+	{
+		newstring[length1] = s2[place];
+		length1++;
+	}
+}
+
+
+else
+{
+	for (place = 0; place < length2; place++)
+	{
+		newstring[length1] = s2[place];
+		length1++;
+	}
+}
+newstring[length1] = 0;
+return (newstring);
+}
