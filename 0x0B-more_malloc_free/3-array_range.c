ad61e8a2696dc3f734d479384acdf507ae2d1b0a
talktobrent <talktobrent@gmail.com>
2018-07-20 03:04:53 +0000

    Created function

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* array_range - creates array of integers
+* @min: lowest integer
+* @max: highest integer
+* Description: Creates array of size max minus min,
+* with min thru max values initialized within
+* Return: pointer to array of allocated memory
+*/
+
+int *array_range(int min, int max)
+{
+int *array;
+int count;
+
+count = 0;
+
+if (min > max)
+	return (NULL);
+
+	array = malloc(sizeof(int) * (max - min));
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	while (count < max - min)
+	{
+		array[count] = min + count;
+		count++;
+	}
+
+return (array);
+}
+
+
