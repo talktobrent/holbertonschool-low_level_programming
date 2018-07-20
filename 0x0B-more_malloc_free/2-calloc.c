d77416501a075af2d6eb8e26eb70b1e6615485e5
talktobrent <talktobrent@gmail.com>
2018-07-20 01:54:42 +0000

    Created function

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+* _calloc - allocates an array to memory, with each value zero'ed out
+* @nmemb: elements to allocate
+* @size: size of each element
+* Return: pointer to array of allocated memory
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int *array;
+unsigned int count;
+void *ptr;
+
+if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	array = malloc(size * nmemb);
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	while (count < nmemb)
+	{
+		array[count] = 0;
+		count++;
+	}
+
+ptr = array;
+
+
+return (ptr);
+}
+
+
