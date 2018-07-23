d71e301d146067c98698160c167efe15cb922d51
talktobrent <talktobrent@gmail.com>
2018-07-23 20:20:53 +0000

    Created main

---
+++ 0x0C-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#define PI 3.14159265359
+
+#endif /* _HOLBERTON_H_ */
---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints file this program was compiled from
+* Return: always zero
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
