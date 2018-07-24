635db2bc5e303c4cd25dad8e8567dd2abfd08717
talktobrent <talktobrent@gmail.com>
2018-07-24 02:00:12 +0000

    Added functions

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -19,4 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 #endif /* _DOG_H_ */
