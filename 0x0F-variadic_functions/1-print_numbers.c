c7d9705c539e7151634cccd6ca5937a513442698
talktobrent <talktobrent@gmail.com>
2018-07-27 21:20:18 +0000

    Created function

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* print_numbers - prints any numbers given
+* @separator: char string used to separate output between numbers
+*/
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int count;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (count = 0; count < n; count++)
+	{
+		if (separator != NULL && count > 0)
+			printf("%s", separator);
+
+		printf("%i", va_arg(args, int));
+	}
+	printf("\n");
+	va_end(args);
+}
+
+
