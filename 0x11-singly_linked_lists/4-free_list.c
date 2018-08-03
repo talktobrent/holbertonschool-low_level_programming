35d220e51bb56dc1484a9c46ca55708a3419d943
talktobrent <talktobrent@gmail.com>
2018-08-03 02:46:36 +0000

    Created function

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * free_list - frees a list_t
+  * @head: pointer to start of linked list
+  */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+		while (head != NULL)
+		{
+			tmp = head;
+			printf("tmp = head");
+			free(head->str);
+			head = head->next;
+			free(tmp);
+		}
+}
