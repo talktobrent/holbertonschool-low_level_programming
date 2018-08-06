d1f56383e5b494a0bad0098a48c48d4148abc062
talktobrent <talktobrent@gmail.com>
2018-08-06 22:26:19 +0000

    Created function

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a listint_t list
+  * @head: pointer to top of list
+  * Description: frees list, then sets 'head' to NULL
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *mover;
+
+	tmp = NULL;
+	mover = *head;
+
+	while (mover != NULL)
+	{
+		tmp = mover;
+		mover = mover->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+
+
+
+}
