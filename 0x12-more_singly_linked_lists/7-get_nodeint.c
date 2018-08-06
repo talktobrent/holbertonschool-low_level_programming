ddbcbc27e0cc1564cd612e72e10e7981d80386ab
talktobrent <talktobrent@gmail.com>
2018-08-06 23:38:40 +0000

    Created function

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - finds desired node in a listint_t linked list
+  * @head: pointer to top of list
+  * @index: node in linked list to find (index starts as 0)
+  * Return: pointer to found node
+  */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+	listint_t *mover;
+
+	count = 0;
+
+	mover = head;
+
+	while (count < index)
+	{
+		mover = mover->next;
+		count++;
+	}
+
+	return (mover);
+}
