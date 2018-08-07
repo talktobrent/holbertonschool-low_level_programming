c9e673d2af304d65ef59e96b52561faab6250c45
talktobrent <talktobrent@gmail.com>
2018-08-07 00:04:01 +0000

    Added 'if' condition inside 'while' loop to account for node index that does not exist

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -20,6 +20,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (count < index)
 	{
+		if (mover->next == NULL)
+			return (NULL);
 		mover = mover->next;
 		count++;
 	}
