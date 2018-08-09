9ae09d30a379f197c743159f4da7822e626a9149
talktobrent <talktobrent@gmail.com>
2018-08-09 22:22:26 +0000

    Created function

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/*
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string given
+ * Return: converted unsigned int
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int pow, decimal;
+	int count;
+
+	count = 0;
+	pow = 1;
+	decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[count] != 0)
+		count++;
+
+	count--;
+
+	while (count >= 0)
+	{
+		if (b[count] != '1' && b[count] != '0')
+			return (0);
+		if (b[count] == '1')
+			decimal = decimal + pow;
+
+		pow = pow + pow;
+		count--;
+	}
+
+	return (decimal);
+}
