37dbefa2cf83cfc7716f885b0b0489c0f1be5cda
talktobrent <talktobrent@gmail.com>
2018-09-14 13:26:33 -0700

    Created function

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints value at all nodes of a linked list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *run;
+	size_t count = 0;
+
+	run = h;
+
+	while (run != NULL)
+	{
+		printf("%d\n", run->n);
+		run = run->next;
+		count++;
+	}
+
+	return (count);
+}
