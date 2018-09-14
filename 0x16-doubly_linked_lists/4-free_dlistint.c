806a2fe9672d56d1856c05b3296576ad54090b80
talktobrent <talktobrent@gmail.com>
2018-09-14 15:21:18 -0700

    Created function

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a linked list
+ * @head: head of linked list
+ */
+
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head->next != NULL)
+	{
+		if (head->prev != NULL)
+			free(head->prev);
+		head = head->next;
+	}
+
+	free(head->prev);
+	free(head);
+
+}
+
