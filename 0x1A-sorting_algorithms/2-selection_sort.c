b2064a536fc876642bce82a195090ff1d759fad5
talktobrent <talktobrent@gmail.com>
2018-10-31 17:13:19 -0700

    Added condition for NULL array

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -11,6 +11,9 @@ void selection_sort(int *array, size_t size)
 	size_t index, run, ilow;
 	int low;
 
+	if (array == NULL)
+		return;
+
 	for (index = 0; index < size; index++)
 	{
 		low = array[index];
