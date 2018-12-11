7434167a0d5f3cfad12102bc2709108f40b2d33f
talktobrent <talktobrent@gmail.com>
2018-12-11 00:44:40 +0000

    Copied mains

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/*
+* binary_tree_node - initiates binary tree node
+* @parent: pointer to parent
+* @value: value at node
+* Returns: new node
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));	
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
