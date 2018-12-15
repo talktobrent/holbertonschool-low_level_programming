453844f0a0e3ed12f82891255cb3198b6a09c5d1
talktobrent <talktobrent@gmail.com>
2018-12-15 03:31:28 -0800

    Added 'crossing' test to set and compare 'top' after crossing into right side of binary tre

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -1,56 +1,58 @@
 #include "binary_trees.h"
 #include <limits.h>
 #define BAD (1 + (long int)INT_MAX)
-#define EMPTY (1 - (long int)INT_MIN)
+#define EMPTY (2 + (long int)INT_MAX)
+#define TOPEMPTY (-1 + (long int)INT_MIN)
+
+long int binary_tree_is_bst_help(const binary_tree_t
+		*tree, long int top, size_t cross);
 
 /**
  * binary_tree_is_bst - finds if binary tree is a valid binary search tree
  * @tree: tree given
- * Return: 1 if truee, 0 if not
+ * Return: 1 if true, 0 if not
  */
-
-long int binary_tree_is_bst_left(const binary_tree_t *tree);
-long int binary_tree_is_bst_right(const binary_tree_t *tree);
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	long int left, right;
-
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL || tree->left == NULL)
-		return (0);
-
-	left = binary_tree_is_bst_left(tree->left);
-	right = binary_tree_is_bst_right(tree->right);
-
-	if (left == right || (left == BAD || right == BAD))
-		return (0);
-
-	if ((tree->left->n < tree->n) && (tree->n < tree->right->n))
-		return (left < tree->n && tree->n < right);
-	return (0);
+	return (binary_tree_is_bst_help(tree, TOPEMPTY, 1) != BAD);
 }
 
 /**
- * binary_tree_is_bst_left - checks left side of tree for correct bst order
+ * binary_tree_is_bst_help - checks left side of tree for correct bst order
  * @tree: tree given
- * Return: left-most value in tree if is higher than all others, INT MAX + 1
- * if not
+ * @top: holds n value of origin node AFTER it crosses over from left, it
+ * is triggered via cross. The value is stored outside INT range by default
+ * until it is crossed, this way it will always compare higher on the left side
+ * @cross: counts recursion to identify origin node
+ * Return: highest value in tree if is true binary tree, INT_MAX + 1 if not
  */
 
-long int binary_tree_is_bst_left(const binary_tree_t *tree)
+long int binary_tree_is_bst_help(const binary_tree_t
+		*tree, long int top, size_t cross)
 {
 	long int left = EMPTY, right = EMPTY;
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (tree->n);
+
 	if (tree->left)
-		left = binary_tree_is_bst_left(tree->left);
+		left = binary_tree_is_bst_help(tree->left, top, cross + 1);
+
+	if (cross == 1)
+		top = tree->n;
 	if (tree->right)
-		right = binary_tree_is_bst_left(tree->right);
+		right = binary_tree_is_bst_help(tree->right, top, cross + 1);
+
 	if (right == BAD || left == BAD)
 		return (BAD);
+
+	if (cross != 1)
+		if (top >= right || top >= left)
+			return (BAD);
+
 	if (!tree->left)
 	{
 		if (right <= tree->n)
@@ -63,43 +65,8 @@ long int binary_tree_is_bst_left(const binary_tree_t *tree)
 			return (BAD);
 		return (left);
 	}
+
 	if (left != right && (left < tree->n && tree->n < right))
 		return (right);
-	return (BAD);
-}
-
-/**
- * binary_tree_is_bst_right - checks right side of tree for correct bst order
- * @tree: tree given
- * Return: left-most value in tree if is higher than all others, INT MAX + 1
- * if not
- */
-
-long int binary_tree_is_bst_right(const binary_tree_t *tree)
-{
-	long int left = EMPTY, right = EMPTY;
-
-	if (tree->right == NULL && tree->left == NULL)
-		return (tree->n);
-	if (tree->left)
-		left = binary_tree_is_bst_right(tree->left);
-	if (tree->right)
-		right = binary_tree_is_bst_right(tree->right);
-	if (right == BAD || left == BAD)
-		return (BAD);
-	if (!tree->left)
-	{
-		if (right <= tree->n)
-			return (BAD);
-		return (right);
-	}
-	if (!tree->right)
-	{
-		if (left >= tree->n)
-			return (BAD);
-		return (left);
-	}
-	if (left != right && (left < tree->n && tree->n < right))
-		return (left);
 	return (BAD);
 }
