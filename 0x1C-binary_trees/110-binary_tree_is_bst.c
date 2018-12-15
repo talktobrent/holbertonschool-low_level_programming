5b54084bc67a7f52a2de392e7a7b8967e53ac3d5
talktobrent <talktobrent@gmail.com>
2018-12-14 22:46:18 -0800

    Created function

---
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -0,0 +1,78 @@
+#include "binary_trees.h"
+#include <limits.h>
+
+/**
+ * binary_tree_is_bst - finds if binary tree is a valid binary search tree
+ * @tree: tree given
+ * Return: 1 if truee, 0 if not
+ */
+
+long int binary_tree_is_bst_left(const binary_tree_t *tree);
+long int binary_tree_is_bst_right(const binary_tree_t *tree);
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	long int left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL || tree->left == NULL)
+		return (0);
+	left = binary_tree_is_bst_left(tree->left);
+	right = binary_tree_is_bst_right(tree->right);
+	if (left > INT_MAX || right > INT_MAX)
+		return (0);
+	return (left < tree->n && tree->n < right);
+}
+
+/**
+ * binary_tree_is_bst_left - checks left side of tree for correct bst order
+ * @tree: tree given
+ * Return: left-most value in tree if is higher than all others, INT MAX + 1
+ * if not
+ */
+
+long int binary_tree_is_bst_left(const binary_tree_t *tree)
+{
+	long int left, right;
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (tree->n);
+	if (tree->left)
+		left = binary_tree_is_bst_left(tree->left);
+	if (tree->right)
+		right = binary_tree_is_bst_left(tree->right);
+	if (!tree->left)
+		return (right);
+	if (!tree->right)
+		return (left);
+	if (left < tree->n && tree->n < right)
+		return (right);
+	return (1 + (long int)INT_MAX);
+}
+
+/**
+ * binary_tree_is_bst_right - checks right side of tree for correct bst order
+ * @tree: tree given
+ * Return: left-most value in tree if is higher than all others, INT MAX + 1
+ * if not
+ */
+
+long int binary_tree_is_bst_right(const binary_tree_t *tree)
+{
+	long int left, right;
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (tree->n);
+	if (tree->left)
+		left = binary_tree_is_bst_right(tree->left);
+	if (tree->right)
+		right = binary_tree_is_bst_right(tree->right);
+	if (!tree->left)
+		return (right);
+	if (!tree->right)
+		return (left);
+	if (left < tree->n && tree->n < right)
+		return (left);
+	return (1 + (long int)INT_MAX);
+}
