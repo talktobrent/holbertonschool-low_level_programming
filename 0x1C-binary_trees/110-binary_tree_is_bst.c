6e0dfb963b341e26c7beb5883d01e0f745ea546f
talktobrent <talktobrent@gmail.com>
2018-12-15 00:21:16 -0800

    Now pass BAD value thru all resursion when found

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 #include <limits.h>
+#define BAD (1 + (long int)INT_MAX)
+#define EMPTY (1 - (long int)INT_MIN)
 
 /**
  * binary_tree_is_bst - finds if binary tree is a valid binary search tree
@@ -22,7 +24,7 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	left = binary_tree_is_bst_left(tree->left);
 	right = binary_tree_is_bst_right(tree->right);
 
-	if (left == right || (left > INT_MAX || right > INT_MAX))
+	if (left == right || (left == BAD || right == BAD))
 		return (0);
 
 	if ((tree->left->n < tree->n) && (tree->n < tree->right->n))
@@ -39,7 +41,7 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 
 long int binary_tree_is_bst_left(const binary_tree_t *tree)
 {
-	long int left, right;
+	long int left = EMPTY, right = EMPTY;
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (tree->n);
@@ -47,21 +49,23 @@ long int binary_tree_is_bst_left(const binary_tree_t *tree)
 		left = binary_tree_is_bst_left(tree->left);
 	if (tree->right)
 		right = binary_tree_is_bst_left(tree->right);
+	if (right == BAD || left == BAD)
+		return (BAD);
 	if (!tree->left)
 	{
 		if (right <= tree->n)
-			return (1 + (long int)INT_MAX);
+			return (BAD);
 		return (right);
 	}
 	if (!tree->right)
 	{
 		if (left >= tree->n)
-			return (1 + (long int)INT_MAX);
+			return (BAD);
 		return (left);
 	}
 	if (left != right && (left < tree->n && tree->n < right))
 		return (right);
-	return (1 + (long int)INT_MAX);
+	return (BAD);
 }
 
 /**
@@ -73,7 +77,9 @@ long int binary_tree_is_bst_left(const binary_tree_t *tree)
 
 long int binary_tree_is_bst_right(const binary_tree_t *tree)
 {
-	long int left, right;
+	long int left = EMPTY, right = EMPTY;
+
+	printf("inhere %i\n", tree->n);
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (tree->n);
@@ -81,19 +87,22 @@ long int binary_tree_is_bst_right(const binary_tree_t *tree)
 		left = binary_tree_is_bst_right(tree->left);
 	if (tree->right)
 		right = binary_tree_is_bst_right(tree->right);
+	printf("im %i left is %li right is %li\n", tree->n, left, right);
+	if (right == BAD || left == BAD)
+		return (BAD);
 	if (!tree->left)
 	{
 		if (right <= tree->n)
-			return (1 + (long int)INT_MAX);
+			return (BAD);
 		return (right);
 	}
 	if (!tree->right)
 	{
 		if (left >= tree->n)
-			return (1 + (long int)INT_MAX);
+			return (BAD);
 		return (left);
 	}
 	if (left != right && (left < tree->n && tree->n < right))
 		return (left);
-	return (1 + (long int)INT_MAX);
+	return (BAD);
 }
