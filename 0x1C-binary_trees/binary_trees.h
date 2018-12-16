7de8ab583e416d4910263bcc1b5b9b5f96be8a27
talktobrent <talktobrent@gmail.com>
2018-12-16 05:21:13 -0800

    Updated header with heap

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -51,5 +51,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
-
+int binary_tree_is_heap(const binary_tree_t *tree);
 #endif
